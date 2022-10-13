2c24630be0af364f889e6d189c966984aa6799b7
Thuy Dung <dungntt@opentechiz.com>
2022-10-13 16:13:12 +0700

    ConfigLambda

--- src/main/java/Page_Element/Header_Element.java
+++ src/main/java/Page_Element/Header_Element.java
@@ -3,22 +3,27 @@ package Page_Element;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class Header_Element {
 //    WebDriver driver;
-    ChromeDriver driver;
-    @FindBy(xpath = "//span[contains(text(),'Log In/Sign Up')]") public WebElement headerLogin;
+  //  ChromeDriver driver;
+    RemoteWebDriver driver;
+    @FindBy(xpath = "//span[@class='authorization-text']") public WebElement headerLogin;
 
-//    @FindBy(xpath = "//span[contains(text(),'Log In/Sign Up')]") public WebElement headerLogin;
-//    @FindBy(xpath = "//span[contains(text(),'Log In/Sign Up')]") public WebElement headerLogin;
+    @FindBy(xpath = "//input[@id='search']") public WebElement headerSearch;
+
+    @FindBy(xpath = "(//button[@title='Search'])[2]") public WebElement headerBtnSearch;
+
+    @FindBy(xpath = "(//span[contains(text(),'Next')])[1]") public WebElement headerBtnNext;
 
 
-
-    public Header_Element(ChromeDriver driver) {
+    public Header_Element(RemoteWebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver, this);
+
     }
 
 
--- src/test/java/Common_Page/AbstractBase_Test.java
+++ src/test/java/Common_Page/AbstractBase_Test.java
@@ -4,18 +4,46 @@ import Page_Lib.App_Lib;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class AbstractBase_Test {
-    ChromeDriver driver;
-    private App_Lib app;
+   // ChromeDriver driver;
+    private App_Lib App;
+
+    String username = "webmasterglamira-group";
+    String accesskey = "Zc41qUZTrlgQTWKjCGAuPpsFrZiCNW8yyOmbo9pY9e8V5TIyXP";
+    static RemoteWebDriver driver = null;
+    String gridURL = "@hub.lambdatest.com/wd/hub";
+    private void setup() {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("browserName", "chrome");
+        capabilities.setCapability("version", "70.0");
+        capabilities.setCapability("platform", "win11"); // If this cap isn't specified, it will just get any available one.
+        capabilities.setCapability("build", "LambdaTest_Dung");
+        capabilities.setCapability("name", "LambdaTestJava_Dung");
+        try {
+            driver = new RemoteWebDriver(new URL("https://" + username + ":" + accesskey + gridURL), capabilities);
+        } catch (MalformedURLException e) {
+            System.out.println("Invalid grid URL");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
     @BeforeTest
     public void setUp() throws Exception {
+        setup();
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        app = new App_Lib(driver);
+          //  driver = new ChromeDriver();
+        App = new App_Lib(driver);
+
+
     }
 
     @AfterTest
@@ -24,7 +52,7 @@ public class AbstractBase_Test {
     }
 
     public App_Lib App() {
-        return app;
+        return App;
     }
 
 }
