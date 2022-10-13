e91dea366fe5d2eed92fbbf31f6d22088934c18e
Ngoc Nguyen <ngocnguyenthi@tcom.vn>
2022-10-13 11:02:11 +0700

    Update_login/logout

--- src/test/java/Tests/LogIn_LogOut_Test.java
+++ src/test/java/Tests/LogIn_LogOut_Test.java
@@ -3,68 +3,38 @@ package Tests;
 import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.checkerframework.checker.index.qual.PolyUpperBound;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeTest;
+
 public class LogIn_LogOut_Test {
-    static WebDriver driver;
+    public static WebDriver driver;
 
-    public void Setup() {
-//        WebDriverManager.chromedriver().setup();
-        System.setProperty("webdriver.chrome.driver", "D:\\Setup\\chromedriver.exe");
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    @BeforeClass
+    public static void Setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.navigate().to("https://dev3.glamira.com/glgb/");
         driver.manage().window().maximize();
-
-        driver.navigate().to("https://dev1.glamira.com/glgb");
-    }
-    @Test
-    public void loginLocationElement() {
-        WebDriver driver = new ChromeDriver();
-        driver.findElement(By.xpath("//li[@data-label='or']//a")).click();
-    }
-    @Test
-    public void status() {
-        boolean status = driver.findElement(By.xpath("//h2[@id='block-customer-login-heading']")).isDisplayed();
-        if (status) {
-            System.out.println("This is form login !");
-        }
-    }
-    @Test
-    public void useNamElement() {
-        driver.findElement(By.xpath("//input[@id='email']")).sendKeys("nguyenthilien1962@gmail.com");
-    }
-    @Test
-    public void passWordElement() {
-        driver.findElement(By.xpath("//field-set[@class='field-set login']//input[@id='pass']")).sendKeys("Lien1962@");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
-    @Test
-    public void actionElement() {
-        driver.findElement(By.xpath("//button[@id='send2']")).submit();
-    }
+   @Test
+   public void SelectElement (){
+        driver.findElement(By.xpath("//a[@title='Log In/Sign Up']")).click();
+   }
 
-    //
-    @Test
-    public void checkdisplayElement() {
-        boolean status1 = driver.findElement(By.linkText("My Orders")).isDisplayed();
-        if (status1) {
-            System.out.println("This is my order");
-        }
-    }
-    @Test
-    public void SelectItem() {
-        driver.findElement(By.linkText("My Orders")).click();
-    }
     //		checkblock
     @AfterClass
     public void editProgram() {
         driver.quit();
     }
-
-
 }
