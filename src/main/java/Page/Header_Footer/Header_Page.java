20c25e880790b8b553faa4a9139a4eceb67f7726
Ngoc Nguyen <ngocnguyenthi@tcom.vn>
2022-10-19 10:37:14 +0700

    update

--- src/main/java/Page/Header_Footer/Header_Page.java
+++ src/main/java/Page/Header_Footer/Header_Page.java
@@ -1,50 +1,27 @@
 package Page.Header_Footer;
 
 import Page_Element.Header_Footer.Header_Element;
+import Page_Lib.ReadData;
 import org.openqa.selenium.remote.RemoteWebDriver;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.Properties;
 
 public class Header_Page {
     //  ChromeDriver driver;
     RemoteWebDriver driver;
     Header_Element headerElement;
+    ReadData data;
     public Header_Page(RemoteWebDriver driver) {
         this.driver = driver;
         headerElement = new Header_Element(driver);
-        driver.manage().window().maximize();
-
+        data = new ReadData();
     }
     public void clickBtnLogIn() {
 
         headerElement.headerLogin.click();
     }
-
     public void senKeySearch() {
 
-        File file = new File("D:\\automation\\src\\main\\Data_Test\\data.properties");
-
-        FileInputStream fileInput = null;
-        try {
-            fileInput = new FileInputStream(file);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        Properties prop = new Properties();
-
-        //load properties file
-        try {
-            prop.load(fileInput);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        headerElement.headerSearch.sendKeys(prop.getProperty("searchKey"));
+        headerElement.headerSearch.sendKeys(data.getKey("searchKey"));
     }
-
     public void clickBtnSearch() {
         headerElement.headerBtnSearch.click();
     }
