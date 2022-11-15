ab280c2f363fe93dfa3f2dcf4f03aff87503492d
Ngoc Nguyen <ngocnguyenthi@tcom.vn>
2022-11-15 17:49:19 +0700

    #6_ Update Checkout, Update ListingPage. + add new case .

--- src/main/java/page/checkout/LoginAndAddressPage.java
+++ src/main/java/page/checkout/LoginAndAddressPage.java
@@ -16,20 +16,17 @@ public class LoginAndAddressPage extends BasePage {
         keyword.sendKeys(PropertiesFile.getPropValue(inputSendKey),
                 PropertiesFile.getPropValue(senKey));
     }
-
-    public void enterDataPhoneFail1(String dataCheck, String inputPhone, String dataPhone1, String dataPass) throws InterruptedException {
-        keyword.webDriverWaitForElementPresent(PropertiesFile.getPropValue(dataCheck), 20);
-        keyword.clearText(PropertiesFile.getPropValue(inputPhone));
-        Thread.sleep(2000);
-        keyword.sendKeys((PropertiesFile.getPropValue(inputPhone)), PropertiesFile.getPropValue(dataPhone1));
+    //    senKey function share
+    public void senKey(String senkey, String getPropValue) {
         keyword.imWait(2);
-        keyword.webDriverWaitForElementPresent((PropertiesFile.getPropValue(dataCheck)), 20);
-        keyword.clearText(PropertiesFile.getPropValue(inputPhone));
-        Thread.sleep(1000);
-        keyword.sendKeys(PropertiesFile.getPropValue(inputPhone), PropertiesFile.getPropValue(dataPass));
-        Thread.sleep(1000);
+        keyword.sendKeys(PropertiesFile.getPropValue(senkey),
+                PropertiesFile.getPropValue(getPropValue));
     }
-
+    //   check webDriver Wait For Element Present
+    public void webDriverWaitForElementPresent(String dataCheck) {
+        keyword.webDriverWaitForElementPresent(PropertiesFile.getPropValue(dataCheck), 20);
+    }
+    //Choose any product to pay
     public void goToPageProduct() throws InterruptedException {
         keyword.imWait(3);
         keyword.navigateToUrl(PropertiesFile.getPropValue("URL_NOTLOGIN"));
@@ -45,12 +42,12 @@ public class LoginAndAddressPage extends BasePage {
         keyword.scrollDownToElement(PropertiesFile.getPropValue("CHECKOUT_SELECT_ONE_SIZE"));
         keyword.click(PropertiesFile.getPropValue("CHECKOUT_SELECT_ONE_SIZE"));
     }
-
-    public void clickBtnSubmit() throws InterruptedException {
-        keyword.click(PropertiesFile.getPropValue("CHECKOUT_PROCEED"));
+    //  button submit
+    public void clickButtonSubmit(String buttonSubmit) {
+        keyword.click(PropertiesFile.getPropValue(buttonSubmit));
     }
 
-
+    //  button product
     public void clickButtonAddProduct() {
         keyword.imWait(5);
         keyword.doubleClick(PropertiesFile.getPropValue("CHECK_OUT_BTN_ADD_PRODUCT"));
@@ -64,59 +61,11 @@ public class LoginAndAddressPage extends BasePage {
         keyword.click(PropertiesFile.getPropValue("CECKOUT_BTN_CONTINUE"));
         Thread.sleep(1000);
     }
-
-    public void enterDataUserName() throws InterruptedException {
-        keyword.webDriverWaitForElementPresent(PropertiesFile.getPropValue("CHECKOUT_FORM_LOGIN_ADDRESS"), 20);
-        Thread.sleep(2000);
-        keyword.sendKeys(PropertiesFile.getPropValue("CHECKOUT_DATA_FIRSTNAME"),
-                PropertiesFile.getPropValue("CHECKOUT_DATA_FIRSNAME_ENTER"));
-        keyword.sendKeys(PropertiesFile.getPropValue("CHECKOUT_DATA_LASTNAME"),
-                PropertiesFile.getPropValue("CHECKOUT_DATA_LASTNAME_ENTER"));
-    }
-
-    //enter Data Email
-    public void enterDataEmail() throws InterruptedException {
-        keyword.imWait(2);
-        keyword.sendKeys(PropertiesFile.getPropValue("CHECKOUT_DATA_EMAIL"),
-                PropertiesFile.getPropValue("CHECKOUT_DATA_EMAIL_ENTER"));
-        keyword.sendKeys(PropertiesFile.getPropValue("CHECKOUT_DATA_CONFIRMEMAIL"),
-                PropertiesFile.getPropValue("CHECKOUT_DATA_CONFIRMEMAIL_ENTER"));
-    }
-
-    //----exception Email.
-    public void enterDataEmailFail1() throws InterruptedException {
-        Thread.sleep(2000);
-        keyword.webDriverWaitForElementPresent(PropertiesFile.getPropValue("CHECKOUT_MESSAGE_FAILE_EMAIL02"), 20);
-    }
-
-    public void enterDataEmailFail2(String messageFail) throws InterruptedException {
-        keyword.webDriverWaitForElementPresent(PropertiesFile.getPropValue(messageFail), 20);
-
-        keyword.webDriverWaitForElementPresent(PropertiesFile.getPropValue(messageFail), 20);
-        Thread.sleep(1000);
-    }
-
-    public void AddressValidation(String booxMessage, String btn) throws InterruptedException {
-        keyword.webDriverWaitForElementPresent(PropertiesFile.getPropValue(booxMessage), 20);
-        Thread.sleep(1000);
-        keyword.click(PropertiesFile.getPropValue(btn));
-    }
-
-    //    senKey function share'
-    public void senKey(String senkey, String getPropValue) throws InterruptedException {
-        keyword.sendKeys(PropertiesFile.getPropValue(senkey),
-                PropertiesFile.getPropValue(getPropValue));
-    }
-    
     public void goBack() {
         keyword.scrollDownToElement(PropertiesFile.getPropValue("CHECKOUT_BTN_BACK"));
         keyword.click(PropertiesFile.getPropValue("CHECKOUT_BTN_BACK"));
     }
 
-    //button Shared
-    public void customerLogin() {
-        keyword.click(PropertiesFile.getPropValue("CHECKOUT_BTN_LOGIN_RGT_CUSTOMER"));
-    }
 
     //check Login AngVerify Customer Regis Wit Invalid Email
     public void LoginAngVerifyCustomerRegisWitInvalidhEmail(String verifyEmail, String verifyPass) throws InterruptedException {
@@ -128,10 +77,9 @@ public class LoginAndAddressPage extends BasePage {
                 PropertiesFile.getPropValue("CHECKOUT_DATA_PASSWORD_ENTER"));
 
     }
-
     //Check InvalidEmail email
-    public void InvalidEmail(String message, String inputEmail, String inpputPass) throws InterruptedException {
-        keyword.webDriverWaitForElementPresent(PropertiesFile.getPropValue(message), 5);
+    public void invalidEmail(String message, String inputEmail, String inpputPass)  {
+        keyword.webDriverWaitForElementPresent(PropertiesFile.getPropValue(message), 10);
         keyword.clearText(PropertiesFile.getPropValue(inputEmail));
         keyword.sendKeys(PropertiesFile.getPropValue(inputEmail),
                 PropertiesFile.getPropValue("CHECKOUT_DATA_EMAIL_ENTER"));
--- src/main/java/test/checkout/LoginAndAddressTest.java
+++ src/main/java/test/checkout/LoginAndAddressTest.java
@@ -34,126 +34,83 @@ public class LoginAndAddressTest extends BaseTest {
     }
 
     @Test(priority = 2, description = "manipulate the address input form confirm the fields")
-    public void testCase_LA_05() throws InterruptedException {
-        objLoginAdrPage.clickBtnSubmit();
+    public void testCase_LA_05()  {
+        objLoginAdrPage.clickButtonSubmit("CHECKOUT_PROCEED");
     }
 
-    @Test(priority = 3, description = "enter exceptions")
+    @Test(priority = 3, description = "Enter all Data Login Address")
     public void testCase_LA06() throws InterruptedException {
         Thread.sleep(1000);
-        objLoginAdrPage.enterDataUserName();
-        objLoginAdrPage.enterDataEmail();
-        objLoginAdrPage.senKey(
-                "CHECKOUT_DATA_PHONE",
-                "CHECKOUT_DATA_PHONE_ENTER_FAIL"
+        objLoginAdrPage.webDriverWaitForElementPresent("CHECKOUT_FORM_LOGIN_ADDRESS"
         );
-        objLoginAdrPage.senKey(
-                "CHECKOUT_DATA_COMPANY",
-                "CHECKOUT_DATA_COMPANY_ENTER"
-        );
-        objLoginAdrPage.senKey(
-                "CHECKOUT_DATA_STREET",
-                "CHECKOUT_DATA_STREET_ENTER"
-        );
-        objLoginAdrPage.senKey(
-                "CHECKOUT_DATA_ZIPCODE",
-                "CHECKOUT_DATA_ZIPCODE_ENTER"
-        );
-        objLoginAdrPage.senKey(
-                "CHECKOUT_DATA_CITY",
-                "CHECKOUT_DATA_CITY_ENTER"
-        );
-        objLoginAdrPage.clickBtnSubmit();
+        objLoginAdrPage.senKey("CHECKOUT_DATA_FIRSTNAME", "CHECKOUT_DATA_FIRSNAME_ENTER");
+        objLoginAdrPage.senKey("CHECKOUT_DATA_LASTNAME", "CHECKOUT_DATA_LASTNAME_ENTER");
+        objLoginAdrPage.senKey("CHECKOUT_DATA_EMAIL", "CHECKOUT_DATA_EMAIL_ENTER");
+        objLoginAdrPage.senKey("CHECKOUT_DATA_CONFIRMEMAIL", "CHECKOUT_DATA_CONFIRMEMAIL_ENTER");
+        objLoginAdrPage.senKey("CHECKOUT_DATA_PHONE", "CHECKOUT_DATA_PHONE_ENTER_FAIL");
+        objLoginAdrPage.senKey("CHECKOUT_DATA_COMPANY", "CHECKOUT_DATA_COMPANY_ENTER");
+        objLoginAdrPage.senKey("CHECKOUT_DATA_STREET", "CHECKOUT_DATA_STREET_ENTER");
+        objLoginAdrPage.senKey("CHECKOUT_DATA_ZIPCODE", "CHECKOUT_DATA_ZIPCODE_ENTER");
+        objLoginAdrPage.senKey("CHECKOUT_DATA_CITY", "CHECKOUT_DATA_CITY_ENTER");
+        objLoginAdrPage.clickButtonSubmit("CHECKOUT_PROCEED");
     }
 
     @Test(priority = 4, description = "Enter Email Fail (wrong format)")
     public void testCase_LA07() throws InterruptedException {
-        objLoginAdrPage.enterDataEmailFail1();
-        objLoginAdrPage.clearTextAndSendKey(
-                "CHECKOUT_DATA_CONFIRMEMAIL",
-                "CHECKOUT_DATA_CONFIRMEMAIL",
-                "CHECKOUT_DATA_EMAIL_FAILD_2");
-        objLoginAdrPage.clickBtnSubmit();
+        objLoginAdrPage.webDriverWaitForElementPresent("CHECKOUT_MESSAGE_FAILE_EMAIL02");
+        objLoginAdrPage.clearTextAndSendKey("CHECKOUT_DATA_CONFIRMEMAIL", "CHECKOUT_DATA_CONFIRMEMAIL", "CHECKOUT_DATA_EMAIL_FAILD_2");
+        objLoginAdrPage.clickButtonSubmit("CHECKOUT_PROCEED");
     }
 
     @Test(priority = 5, description = "")
     public void testCase_LA08() throws InterruptedException {
-        objLoginAdrPage.enterDataEmailFail2("CHECKOUT_MESSAGE_FAILE_EMAIL03");
-        objLoginAdrPage.clearTextAndSendKey(
-                "CHECKOUT_DATA_CONFIRMEMAIL",
-                "CHECKOUT_DATA_CONFIRMEMAIL",
-                "CHECKOUT_DATA_EMAIL_FAILD_1");
-        objLoginAdrPage.clearTextAndSendKey("CHECKOUT_DATA_CONFIRMEMAIL",
-                "CHECKOUT_DATA_CONFIRMEMAIL",
-                "CHECKOUT_DATA_EMAIL_ENTER"
-        );
+        objLoginAdrPage.webDriverWaitForElementPresent("CHECKOUT_MESSAGE_FAILE_EMAIL03");
+        objLoginAdrPage.clearTextAndSendKey("CHECKOUT_DATA_CONFIRMEMAIL", "CHECKOUT_DATA_CONFIRMEMAIL", "CHECKOUT_DATA_EMAIL_FAILD_1");
+        objLoginAdrPage.webDriverWaitForElementPresent("CHECKOUT_MESSAGE_FAILE_EMAIL03");
+        objLoginAdrPage.clearTextAndSendKey("CHECKOUT_DATA_CONFIRMEMAIL", "CHECKOUT_DATA_CONFIRMEMAIL", "CHECKOUT_DATA_EMAIL_ENTER");
     }
 
     @Test(priority = 6, description = "Enter Phone Fail (wrong format) ")
     public void testCase_LA09() throws InterruptedException {
-        objLoginAdrPage.enterDataPhoneFail1
-                (
-                        "CHECKOUT_DATA_MESSAGE_PHONE",
-                        "CHECKOUT_DATA_PHONE",
-                        "CHECKOUT_DATA_PHONE_ENTER_FAIL01",
-                        "CHECKOUT_DATA_PHONE_ENTER_SUCCESS01"
-                );
-        objLoginAdrPage.clickBtnSubmit();
-        objLoginAdrPage.AddressValidation
-                (
-                        "CHECKOUT_BOOX_MESSAGE_FAIL",
-                        "CHECKOUT_BTN_CANCEL");
-
-        objLoginAdrPage.clearTextAndSendKey(
-                "CHECKOUT_DATA_STREET",
-                "CHECKOUT_DATA_STREET",
-                "CHECKOUT_DATA_STREET_ENTER01"
-        );
-        objLoginAdrPage.clearTextAndSendKey(
-                "CHECKOUT_DATA_ZIPCODE",
-                "CHECKOUT_DATA_ZIPCODE",
-                "CHECKOUT_DATA_ZIPCODE_ENTER01"
-        );
-        objLoginAdrPage.clearTextAndSendKey(
-                "CHECKOUT_DATA_CITY",
-                "CHECKOUT_DATA_CITY",
-                "CHECKOUT_DATA_CITY_ENTER01"
-        );
-        objLoginAdrPage.clickBtnSubmit();
+        objLoginAdrPage.webDriverWaitForElementPresent("CHECKOUT_DATA_MESSAGE_PHONE");
+        objLoginAdrPage.clearTextAndSendKey("CHECKOUT_DATA_PHONE", "CHECKOUT_DATA_PHONE", "CHECKOUT_DATA_PHONE_ENTER_FAIL01");
+        objLoginAdrPage.webDriverWaitForElementPresent("CHECKOUT_DATA_MESSAGE_PHONE");
+        objLoginAdrPage.clearTextAndSendKey("CHECKOUT_DATA_PHONE", "CHECKOUT_DATA_PHONE", "CHECKOUT_DATA_PHONE_ENTER_SUCCESS01");
+        objLoginAdrPage.clickButtonSubmit("CHECKOUT_PROCEED");
+        objLoginAdrPage.webDriverWaitForElementPresent("CHECKOUT_BOOX_MESSAGE_FAIL");
+        Thread.sleep(2000);
+        objLoginAdrPage.clickButtonSubmit("CHECKOUT_BTN_CANCEL");
+        objLoginAdrPage.clearTextAndSendKey("CHECKOUT_DATA_STREET", "CHECKOUT_DATA_STREET", "CHECKOUT_DATA_STREET_ENTER01");
+        objLoginAdrPage.clearTextAndSendKey("CHECKOUT_DATA_ZIPCODE", "CHECKOUT_DATA_ZIPCODE", "CHECKOUT_DATA_ZIPCODE_ENTER01");
+        objLoginAdrPage.clearTextAndSendKey("CHECKOUT_DATA_CITY", "CHECKOUT_DATA_CITY", "CHECKOUT_DATA_CITY_ENTER01");
+        objLoginAdrPage.clickButtonSubmit("CHECKOUT_PROCEED");
         Thread.sleep(7000);
     }
 
     @Test(priority = 8, description = "Customer login\n")
-    public void testCase_LA18()  {
+    public void testCase_LA18() {
         objLoginAdrPage.goBack();
     }
 
     @Test(priority = 9, description = "manipulate the login form without entering information")
-    public void testCase_LA01()  {
-        objLoginAdrPage.customerLogin();
+    public void testCase_LA01() {
+        objLoginAdrPage.clickButtonSubmit("CHECKOUT_BTN_LOGIN_RGT_CUSTOMER");
     }
 
     @Test(priority = 10, description = "Customer Login - Enter Data Email Error")
     public void testCase_LA2() throws InterruptedException {
-        objLoginAdrPage.LoginAngVerifyCustomerRegisWitInvalidhEmail(
-                "CHECKOUT_MESSAGE_EMAIL_ERROR",
-                "CHECKOUT_MESSAGE_PASSW_ERROR");
-        objLoginAdrPage.customerLogin();
+        objLoginAdrPage.LoginAngVerifyCustomerRegisWitInvalidhEmail("CHECKOUT_MESSAGE_EMAIL_ERROR", "CHECKOUT_MESSAGE_PASSW_ERROR");
+        objLoginAdrPage.clickButtonSubmit("CHECKOUT_BTN_LOGIN_RGT_CUSTOMER");
     }
 
     @Test(priority = 11, description = "re-enter email and password until correct")
     public void testCase_LA3() throws InterruptedException {
-        objLoginAdrPage.InvalidEmail
-                (
-                        "CHECKOUT_MESSAGE_INVALID_LOGIN_OR_PASSW",
-                        "CHECKOUT_DATA_EMAIL_ADDRESS",
-                        "CHECKOUT_DATA_PASSWORD"
-                );
-        objLoginAdrPage.customerLogin();
+        objLoginAdrPage.invalidEmail("CHECKOUT_MESSAGE_INVALID_LOGIN_OR_PASSW", "CHECKOUT_DATA_EMAIL_ADDRESS", "CHECKOUT_DATA_PASSWORD");
+        objLoginAdrPage.clickButtonSubmit("CHECKOUT_BTN_LOGIN_RGT_CUSTOMER");
     }
 
     @Test(priority = 12, description = "aa")
-    public void testCase_LA04()  {
+    public void testCase_LA04() {
 
     }
 }
