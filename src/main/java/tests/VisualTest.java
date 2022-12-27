28644f58b1127a9c6cff2ecb2a5cbb6e11ece876
linh <linh@onlinebizsoft.com>
2022-12-27 17:49:29 +0700

    #9 Update UI Testing

--- src/main/java/tests/VisualTest.java
+++ src/main/java/tests/VisualTest.java
@@ -4,10 +4,9 @@ public class VisualTest extends BaseTest {
     @Test
     public void visualTest() throws Exception {
         steps
-                .A();
-                //.givenIAcceptTheCookies()
-//                .givenITakeScreenShot()
-//                .whenISaveTheScreenShotsToFolders()
-//                .thenIShouldCompareScreenshotsSuccessfully();
+                .givenIAcceptTheCookies()
+                .givenITakeScreenShot()
+                .whenISaveTheScreenShotsToFolders()
+                .thenIShouldCompareScreenshotsSuccessfully();
     }
 }
