62e891975954f41477ba224b48ead79336aa05dc
linh <linh@onlinebizsoft.com>
2022-12-28 15:44:21 +0700

    #9 Update code UI Testing

--- src/main/java/tests/VisualTest.java
+++ src/main/java/tests/VisualTest.java
@@ -1,10 +1,16 @@
 package tests;
 import org.testng.annotations.Test;
+import steps.Steps2;
+
 public class VisualTest extends BaseTest {
+    private Steps2 step2;
     @Test
     public void visualTest() throws Exception {
+        step2 = new Steps2(this.keyword);
+        step2.a();
+
         steps
-                .givenIAcceptTheCookies()
+                //.givenIAcceptTheCookies()
                 .givenITakeScreenShot()
                 .whenISaveTheScreenShotsToFolders()
                 .thenIShouldCompareScreenshotsSuccessfully();
