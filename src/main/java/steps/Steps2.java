17a1d7c39b444fbfd652810b42d03d443e5e7ac1
DungAutomation <dungntt@opentechiz.com>
2023-01-10 17:39:00 +0700

    screenShots : My Account, SignIn/SignUp, Return

--- src/main/java/steps/Steps2.java
+++ src/main/java/steps/Steps2.java
@@ -1,17 +1,21 @@
 package steps;
 
 import io.cucumber.java.sl.In;
+import page.home.RegisterPage;
+import page.signinSignup.SignUpPage;
 import tests.BaseTest;
 import core.KeywordWeb;
 import core.PropertiesFile;
 import page.home.LoginPage;
 import core.BasePage;
 
+import java.util.Date;
 import java.util.Properties;
 
 import org.testng.annotations.Test;
 
 public class Steps2 extends BasePage {
+    public RegisterPage regis;
 
     public Steps2() {
         super();
@@ -19,12 +23,15 @@ public class Steps2 extends BasePage {
 
     public Steps2(KeywordWeb key) {
         super(key);
+
     }
 
     public LoginPage objLogin;
+    public SignUpPage objSignIn;
+
 
     public void gotoLogin() throws InterruptedException {
-        objLogin = new LoginPage(this.keyword);
+
         String a = PropertiesFile.getPropValue("CHECKOUT_LA_LBL_CODE");
         System.out.println(a);
         objLogin.loginOnWebsite("LOGIN_DATA_EMAIL", "LOGIN_DATA_PASSWORD",
@@ -98,4 +105,114 @@ public class Steps2 extends BasePage {
         keyword.click("LTP_REMOTE_FILTER");
         filterLeft("LTP_COLOR", "LTP_SELECT_COLOR");
     }
+    public void setUp() throws InterruptedException {
+        regis = new RegisterPage();
+        objLogin = new LoginPage(this.keyword);
+        objSignIn = new SignUpPage(this.keyword);
+        keyword.navigateToUrl("https://dev4.glamira.com/glau/");
+        regis.chooseLanguages();
+        keyword.untilJqueryIsDone(70L);
+        objLogin.loginOnWebsite("linh@onlinebizsoft.com","Linh@123",null,null,true);
+
+    }
+    public void setUp1() throws InterruptedException {
+        regis = new RegisterPage();
+        objLogin = new LoginPage(this.keyword);
+        objSignIn = new SignUpPage(this.keyword);
+        keyword.navigateToUrl("https://dev4.glamira.com/glau/");
+        regis.chooseLanguages();
+        keyword.untilJqueryIsDone(70L);
+    }
+    public void myAccount_Overview_Content() throws InterruptedException {
+        setUp();
+        keyword.navigateToUrl("https://dev4.glamira.com/glau/customer/account/");
+    }
+    public void myAccount_Overview_Footer() throws InterruptedException {
+       keyword.scrollToPositionByScript("window.scrollBy(0,10000)");
+       keyword.untilJqueryIsDone(70L);
+       keyword.waitForElementNotVisible(10,"//div[@class='loading-mask']");
+    }
+    public void return_CustomerExist() throws InterruptedException {
+        setUp();
+        keyword.navigateToUrl("https://dev4.glamira.com/glau/return/order/index/");
+        keyword.untilJqueryIsDone(70L);
+        keyword.waitForElementNotVisible(10,"//div[@class='loading-mask']");
+    }
+    public void return_CustomerNotExist() throws InterruptedException {
+        keyword.click("MAC_BTN_LOGOUT");
+        keyword.click("MAC_LOGOUT_OK");
+        keyword.untilJqueryIsDone(70L);
+        keyword.waitForElementNotVisible(10,"//div[@class='loading-mask']");
+        keyword.navigateToUrl("https://dev4.glamira.com/glau/return");
+        keyword.untilJqueryIsDone(70L);
+        keyword.waitForElementNotVisible(10,"//div[@class='loading-mask']");
+    }
+    public void signUp_FormHomeLogin() throws InterruptedException {
+        setUp1();
+        keyword.click("LOGIN_BTN_LOGIN");
+        keyword.untilJqueryIsDone(70L);
+     //   keyword.waitForElementNotVisible(10,"//div[@class='loading-mask']");
+
+    }
+    public void signUp_FormCreateAccount_Step1() throws InterruptedException {
+        keyword.reLoadPage();
+        objSignIn.goToFormCreateMyAccount();
+    }
+    public void signUp_FormCreateAccountError_Step1() throws InterruptedException {
+        keyword.waitForElementNotVisible(10,"//div[@class='loading-mask']");
+        keyword.click("SIGNUP_BTN_NEXT_STEEP");
+        keyword.untilJqueryIsDone(70L);
+        keyword.waitForElementNotVisible(10,"//div[@class='loading-mask']");
+    }
+    public void signUp_FormCreateAccount_Step2() throws InterruptedException {
+        String firstName = "Thuy";
+        String lastName = "Dung";
+        String timestamp = new java.text.SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
+        String mail = "dung"+timestamp+"@gmail.com";
+        PropertiesFile.serPropValue("COM_INP_DATA_EMAIL_UI",mail);
+        objSignIn.sendKeyFullDataFormInformation(firstName,lastName,mail,mail);
+        keyword.click("SIGNUP_BTN_NEXT_STEEP");
+        keyword.untilJqueryIsDone(70L);
+        keyword.waitForElementNotVisible(10,"//div[@class='loading-mask']");
+
+    }
+    public void signUp_FormCreateAccountError_Step2() throws InterruptedException {
+        keyword.waitForElementNotVisible(10,"//div[@class='loading-mask']");
+        keyword.click("SIGNUP_BTN_CREATE_ACCOUNT");
+        keyword.untilJqueryIsDone(70L);
+        keyword.waitForElementNotVisible(10,"//div[@class='loading-mask']");
+    }
+    public void signUp_FormCreateAccount_Step3() throws InterruptedException {
+        keyword.sendKeys("SIGNUP_PASSWORD_INFORMATION","LOGIN_NEW_PASSWORD");
+        keyword.click("SIGNUP_CHECKBOX_AGREE");
+        keyword.click("SIGNUP_BTN_CREATE_ACCOUNT");
+        keyword.waitForElementNotVisible(10,"//div[@class='loading-mask']");
+
+    }
+    public void signUp_FormCreateAccountError_Step3() throws InterruptedException {
+        keyword.click("SIGNUP_BTN_SUBMIT_ACCOUNT");
+        keyword.untilJqueryIsDone(70L);
+        keyword.waitForElementNotVisible(10,"//div[@class='loading-mask']");
+    }
+    public void sinIn_FormHomeForgotPassword() throws InterruptedException {
+        setUp1();
+        keyword.click("LOGIN_BTN_LOGIN");
+        keyword.hoverAndClick("LOGIN_BTN_FORGOT_PASSWORD");
+        keyword.untilJqueryIsDone(70L);
+        keyword.waitForElementNotVisible(10,"//div[@class='loading-mask']");
+
+    }
+    public void sinIn_FormForgotPassword_Email() throws InterruptedException {
+        keyword.click("LOGIN_BTN_FORGOT_PASSWORD");
+        keyword.waitForElementNotVisible(10,"//div[@class='loading-mask']");
+    }
+    public void sinIn_FormForgotPassword_EmailError() throws InterruptedException {
+        keyword.click("LOGIN_BTN_SUBMIT_FORGOT_PASSWORD");
+        keyword.waitForElementNotVisible(10,"//div[@class='loading-mask']");
+    }
+
+
+
+
+
 }
--- src/main/java/tests/VisualTest.java
+++ src/main/java/tests/VisualTest.java
@@ -3,6 +3,8 @@ package tests;
 import org.testng.annotations.Test;
 import steps.Steps2;
 
+import java.io.IOException;
+
 public class VisualTest extends BaseTest {
     private Steps2 step2;
 
@@ -120,99 +122,215 @@ public class VisualTest extends BaseTest {
                 .thenIShouldCompareScreenshotsSuccessfully();
     }
 
-    @Test(priority = 11)
-    public void listingPage_1() throws Exception {
-        step2.filterListing();
+//    @Test(priority = 11)
+//    public void listingPage_1() throws Exception {
+//        step2.filterListing();
+//        steps
+//                .givenITakeScreenShot()
+//                .whenISaveTheScreenShotsToFolders()
+//                .thenIShouldCompareScreenshotsSuccessfully();
+//    }
+//
+//    @Test(priority = 12)
+//    public void listingPage_2() throws Exception {
+//        step2.filterListingByPerciousStones();
+//        steps
+//                .givenITakeScreenShot()
+//                .whenISaveTheScreenShotsToFolders()
+//                .thenIShouldCompareScreenshotsSuccessfully();
+//    }
+//
+//    @Test(priority = 13)
+//    public void listingPage_3() throws Exception {
+//        step2.filterListingBySemiPreciousStones();
+//        steps
+//                .givenITakeScreenShot()
+//                .whenISaveTheScreenShotsToFolders()
+//                .thenIShouldCompareScreenshotsSuccessfully();
+//    }
+//
+//    @Test(priority = 14)
+//    public void listingPage_4() throws Exception {
+//        step2.filterListingBySwarovskiCrystals();
+//        steps
+//                .givenITakeScreenShot()
+//                .whenISaveTheScreenShotsToFolders()
+//                .thenIShouldCompareScreenshotsSuccessfully();
+//    }
+//
+//    @Test(priority = 15)
+//    public void listingPage_6() throws Exception {
+//        step2.filterListingByCulturedPearls();
+//        steps
+//                .givenITakeScreenShot()
+//                .whenISaveTheScreenShotsToFolders()
+//                .thenIShouldCompareScreenshotsSuccessfully();
+//    }
+//
+//    @Test(priority = 16)
+//    public void listingPage_7() throws Exception {
+//        step2.filterListingByLabCreatedStones();
+//        steps
+//                .givenITakeScreenShot()
+//                .whenISaveTheScreenShotsToFolders()
+//                .thenIShouldCompareScreenshotsSuccessfully();
+//    }
+//
+//    @Test(priority = 17)
+//    public void listingPage_8() throws Exception {
+//        step2.filterListingByCabochonStones();
+//        steps
+//                .givenITakeScreenShot()
+//                .whenISaveTheScreenShotsToFolders()
+//                .thenIShouldCompareScreenshotsSuccessfully();
+//    }
+//
+//    @Test(priority = 18)
+//    public void listingPage_9() throws Exception {
+//        step2.filterListingByCarat();
+//        steps
+//                .givenITakeScreenShot()
+//                .whenISaveTheScreenShotsToFolders()
+//                .thenIShouldCompareScreenshotsSuccessfully();
+//    }
+//        @Test(priority = 19)
+//    public void listingPage_10() throws Exception {
+//        step2.filterListingByCarat();
+//        steps
+//                .givenITakeScreenShot()
+//                .whenISaveTheScreenShotsToFolders()
+//                .thenIShouldCompareScreenshotsSuccessfully();
+//    }
+//        @Test(priority = 20)
+//    public void listingPage_11() throws Exception {
+//        step2.filterListingByStoneShape();
+//        steps
+//                .givenITakeScreenShot()
+//                .whenISaveTheScreenShotsToFolders()
+//                .thenIShouldCompareScreenshotsSuccessfully();
+//    }
+//    @Test(priority = 21)
+//    public void listingPage_12() throws Exception {
+//        step2.filterListingByColour();
+//        steps
+//                .givenITakeScreenShot()
+//                .whenISaveTheScreenShotsToFolders()
+//                .thenIShouldCompareScreenshotsSuccessfully();
+//    }
+
+  //  @Test(priority = 1)
+    public void MyAccount_Overview_Content() throws Exception {
+        step2.myAccount_Overview_Content();
         steps
                 .givenITakeScreenShot()
                 .whenISaveTheScreenShotsToFolders()
                 .thenIShouldCompareScreenshotsSuccessfully();
     }
+  //  @Test(priority = 2)
+    public void MyAccount_Overview_Footer() throws Exception {
+        step2.myAccount_Overview_Footer();
+        steps
+                .givenITakeScreenShot()
+                .whenISaveTheScreenShotsToFolders()
+                .thenIShouldCompareScreenshotsSuccessfully();
+    }
+  //  @Test(priority = 3)
+    public void Return_CustomerExist() throws Exception {
+        step2.return_CustomerExist();
+        steps
+                .givenITakeScreenShot()
+                .whenISaveTheScreenShotsToFolders()
+                .thenIShouldCompareScreenshotsSuccessfully();
 
+    }
+  //  @Test(priority = 4)
+    public void Return_CustomerNotExist() throws Exception {
+        step2.return_CustomerNotExist();
+        steps
+                .givenITakeScreenShot()
+                .whenISaveTheScreenShotsToFolders()
+                .thenIShouldCompareScreenshotsSuccessfully();
+    }
+ //   @Test(priority = 5)
+    public void SignUp_FormHomeLogin() throws Exception {
+        step2.signUp_FormHomeLogin();
+        steps
+                .givenITakeScreenShot()
+                .whenISaveTheScreenShotsToFolders()
+                .thenIShouldCompareScreenshotsSuccessfully();
+    }
+ //   @Test(priority = 6)
+    public void SignUp_FormCreateAccount_Step1() throws Exception {
+        step2.signUp_FormCreateAccount_Step1();
+        steps
+                .givenITakeScreenShot()
+                .whenISaveTheScreenShotsToFolders()
+                .thenIShouldCompareScreenshotsSuccessfully();
+    }
+ //   @Test(priority = 7)
+    public void SignUp_FormCreateAccountError_Step1() throws Exception {
+        step2.signUp_FormCreateAccountError_Step1();
+        steps
+                .givenITakeScreenShot()
+                .whenISaveTheScreenShotsToFolders()
+                .thenIShouldCompareScreenshotsSuccessfully();
+    }
+ //   @Test(priority = 8)
+    public void SignUp_FormCreateAccount_Step2() throws Exception {
+        step2.signUp_FormCreateAccount_Step2();
+        steps
+                .givenITakeScreenShot()
+                .whenISaveTheScreenShotsToFolders()
+                .thenIShouldCompareScreenshotsSuccessfully();
+    }
+  //  @Test(priority = 9)
+    public void SignUp_FormCreateAccountError_Step2() throws Exception {
+        step2.signUp_FormCreateAccountError_Step2();
+        steps
+                .givenITakeScreenShot()
+                .whenISaveTheScreenShotsToFolders()
+                .thenIShouldCompareScreenshotsSuccessfully();
+    }
+ //   @Test(priority = 10)
+    public void SignUp_FormCreateAccount_Step3() throws Exception {
+        step2.signUp_FormCreateAccount_Step3();
+        steps
+                .givenITakeScreenShot()
+                .whenISaveTheScreenShotsToFolders()
+                .thenIShouldCompareScreenshotsSuccessfully();
+    }
+  //  @Test(priority = 11)
+    public void SignUp_FormCreateAccountError_Step3() throws Exception {
+        step2.signUp_FormCreateAccountError_Step3();
+        steps
+                .givenITakeScreenShot()
+                .whenISaveTheScreenShotsToFolders()
+                .thenIShouldCompareScreenshotsSuccessfully();
+    }
     @Test(priority = 12)
-    public void listingPage_2() throws Exception {
-        step2.filterListingByPerciousStones();
+    public void SinIn_FormHomeForgotPassword() throws Exception {
+        step2.sinIn_FormHomeForgotPassword();
         steps
                 .givenITakeScreenShot()
                 .whenISaveTheScreenShotsToFolders()
                 .thenIShouldCompareScreenshotsSuccessfully();
     }
-
     @Test(priority = 13)
-    public void listingPage_3() throws Exception {
-        step2.filterListingBySemiPreciousStones();
+    public void SinIn_FormForgotPassword_Email() throws Exception {
+        step2.sinIn_FormForgotPassword_Email();
         steps
                 .givenITakeScreenShot()
                 .whenISaveTheScreenShotsToFolders()
                 .thenIShouldCompareScreenshotsSuccessfully();
     }
-
     @Test(priority = 14)
-    public void listingPage_4() throws Exception {
-        step2.filterListingBySwarovskiCrystals();
+    public void SinIn_FormForgotPassword_EmailError() throws Exception {
+        step2.sinIn_FormForgotPassword_EmailError();
         steps
                 .givenITakeScreenShot()
                 .whenISaveTheScreenShotsToFolders()
                 .thenIShouldCompareScreenshotsSuccessfully();
     }
 
-    @Test(priority = 15)
-    public void listingPage_6() throws Exception {
-        step2.filterListingByCulturedPearls();
-        steps
-                .givenITakeScreenShot()
-                .whenISaveTheScreenShotsToFolders()
-                .thenIShouldCompareScreenshotsSuccessfully();
-    }
 
-    @Test(priority = 16)
-    public void listingPage_7() throws Exception {
-        step2.filterListingByLabCreatedStones();
-        steps
-                .givenITakeScreenShot()
-                .whenISaveTheScreenShotsToFolders()
-                .thenIShouldCompareScreenshotsSuccessfully();
-    }
-
-    @Test(priority = 17)
-    public void listingPage_8() throws Exception {
-        step2.filterListingByCabochonStones();
-        steps
-                .givenITakeScreenShot()
-                .whenISaveTheScreenShotsToFolders()
-                .thenIShouldCompareScreenshotsSuccessfully();
-    }
-
-    @Test(priority = 18)
-    public void listingPage_9() throws Exception {
-        step2.filterListingByCarat();
-        steps
-                .givenITakeScreenShot()
-                .whenISaveTheScreenShotsToFolders()
-                .thenIShouldCompareScreenshotsSuccessfully();
-    }
-        @Test(priority = 19)
-    public void listingPage_10() throws Exception {
-        step2.filterListingByCarat();
-        steps
-                .givenITakeScreenShot()
-                .whenISaveTheScreenShotsToFolders()
-                .thenIShouldCompareScreenshotsSuccessfully();
-    }
-        @Test(priority = 20)
-    public void listingPage_11() throws Exception {
-        step2.filterListingByStoneShape();
-        steps
-                .givenITakeScreenShot()
-                .whenISaveTheScreenShotsToFolders()
-                .thenIShouldCompareScreenshotsSuccessfully();
-    }
-    @Test(priority = 21)
-    public void listingPage_12() throws Exception {
-        step2.filterListingByColour();
-        steps
-                .givenITakeScreenShot()
-                .whenISaveTheScreenShotsToFolders()
-                .thenIShouldCompareScreenshotsSuccessfully();
-    }
 }
