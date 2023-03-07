7d6fbcecd4554f1d1a7fef5cc9227dae200b498a
Ngoc Automation <ngocnt@opentechiz.com>
2023-03-07 16:52:06 +0700

    #6_push

--- src/main/java/page/CreateAccountOnMobilePage.java
+++ src/main/java/page/CreateAccountOnMobilePage.java
@@ -3,28 +3,23 @@ package page;
 import core.BasePage;
 import core.KeywordWeb;
 import core.LogHelper;
-import core.PropertiesFile;
 import org.slf4j.Logger;
 import page.home.RegisterPage;
 import page.signinSignup.SignInPage;
 import page.signinSignup.SignUpPage;
-
-import java.util.Date;
 
 public class CreateAccountMobilePage extends BasePage {
     private static Logger logger = LogHelper.getLogger();
     private SignUpPage objSignUp;
     private RegisterPage objRegist;
     private SignInPage objSignIn;
-    private CreateAccountOnStagePage objCreateAccount;
-
+    private CreateAccountOnWebPage objCreateAccount;
     public CreateAccountMobilePage(KeywordWeb key) {
         super(key);
-        objCreateAccount = new CreateAccountOnStagePage(this.keyword);
+        objCreateAccount = new CreateAccountOnWebPage(this.keyword);
         objSignIn = new SignInPage(this.keyword);
         objRegist = new RegisterPage(this.keyword);
     }
-
     public void CreateNewCustomerSuccessfully() throws Exception {
         keyword.untilJqueryIsDone(50L);
         keyword.scrollDownToElement("LOGIN_MENULEFT");
@@ -35,14 +30,12 @@ public class CreateAccountMobilePage extends BasePage {
         keyword.untilJqueryIsDone(50L);
         objCreateAccount.createAndVerifyForm();
     }
-
     public void forgotPassword() throws Exception {
         objCreateAccount.setUpFormForgot();
         objCreateAccount.sendData();
         keyword.resizeBrowser(319, 848);
         keyword.assertEquals("SIGNIN_UPDATE_PASSWORD_SUCCESS", "LOGIN_MESSAGE_RESET_PASSWORD_SUCCESS");
     }
-
     public void forgotPasswordCheckOut() throws Exception {
         objCreateAccount.setupForgot("https://stage.glamira.co.uk/glamira-pendant-elsie.html?alloy=red_white-585&stone1=diamond-Brillan");
         objCreateAccount.sendData();
