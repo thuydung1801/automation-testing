199d10d082d2ae2bbe6d28d5dc05369c7471bc20
Ngoc Automation <ngocnt@opentechiz.com>
2023-04-05 09:23:38 +0700

    update

--- src/main/java/test/LoginReturnFormTest.java
+++ src/main/java/test/LoginReturnFormTest.java
@@ -43,13 +43,13 @@ public class LoginReturnFormTest extends BaseTest {
 
     @Test(priority = 4, description = "Next step return form, login with password not matching email customer")
     public void testCase_LS_02() throws InterruptedException {
-//        objLoginReturn.dataFormLoginReturnForm("DATA_EMAIL_RETURN", "REGIST_DATA_PASSWORD", false, "LA_MESSAGE_INVALID_LOGIN", "MESSAGE_FAIL_RETURN_FORM");
+        objLoginReturn.dataFormLoginReturnForm("DATA_EMAIL_RETURN", "REGIST_DATA_PASSWORD", false, "LA_MESSAGE_INVALID_LOGIN", "MESSAGE_FAIL_RETURN_FORM","");
     }
 
     @Test(priority = 5, description = "Submit Return form, login successfully + Input email or order having space")
     public void testCase_LS_01_03() throws InterruptedException {
 //        setUpLogin();
-//        objLoginReturn.dataFormLoginReturnForm(" " + "DATA_EMAIL_RETURN", "DATA_PASSWORD_LOGIN_RETURN", false, "DATA_LABLE", "STEP_LABLE");
+        objLoginReturn.dataFormLoginReturnForm(" " + "DATA_EMAIL_RETURN", "DATA_PASSWORD_LOGIN_RETURN", false, "DATA_LABLE", "STEP_LABLE","");
     }
 
     @Test(priority = 6, description = "The order having order date before >60days")
@@ -69,10 +69,9 @@ public class LoginReturnFormTest extends BaseTest {
 //        setUpLogin();
         objLoginReturn.orderNotEngraving();
     }
-    //    @Test(priority = 9, description = "Return order with the item has returned")
+        @Test(priority = 9, description = "Return order with the item has returned")
     public void testCase_LS_10() throws InterruptedException {
 //        setUpLogin();
-//        objLoginReturn.goToFormLoginReturn();
         objLoginReturn.orderReturned();
     }
 }
